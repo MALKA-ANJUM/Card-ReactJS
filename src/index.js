402aee6860deee410e52114dbdb8920aad09bdcf
Malka <malkaanjum1999@gmail.com>
2022-08-13 21:38:16 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div className='cards'>
+      <div className='card'>
+        <img src='' alt='myPic' className='card_img'></img>
+        <div className='card_info'>
+          <span className='card_category'></span>
+          <h3 className='card_title'></h3>
+          <a href='' target="_blank">
+            <button>Watc Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
